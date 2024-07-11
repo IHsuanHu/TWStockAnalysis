9e87366415e00e8aff6076c50c6ce585bb2bd5bc
IHsuanHu <ihsuanhu@gmail.com>
2024-07-10 22:24:38 -0400

    v1

---
+++ priceCalculator.py
@@ -0,0 +1,152 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import pandas as pd
+import re
+from collections import defaultdict
+import os
+import datetime
+from openpyxl import load_workbook
+
+def close_window():
+    root.destroy()
+
+def browse_file():
+    fileName = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+    if fileName:
+        processFile(fileName)
+
+def extract_broker_name(broker):
+    """ 提取券商名称，不包含数字代号 """
+    if isinstance(broker, str):
+        # 去掉数字、空格和英文字母
+        broker = re.sub(r'[A-Za-z0-9\s]+', '', broker).strip()
+        return broker
+    return ""
+
+firmBP = defaultdict(int)
+firmSP = defaultdict(int)
+firmBS = defaultdict(int)
+firmSS =defaultdict(int)
+stock_code = ""
+current_date = datetime.datetime.now()
+formatted_date = current_date.strftime("%Y%m%d")
+
+def processFile(filepath):
+    global firmBP, firmSP, firmBS, firmSS, stock_code
+    try:
+        with open(filepath, 'r', encoding='big5') as file:
+            lines = file.readlines()
+            stock_code = lines[1].split(',')[1].strip()  # 获取股票代码
+
+        df = pd.read_csv(filepath, encoding="big5", header=1, skiprows=1)
+  
+        for i, row in df.iterrows():
+            broker = extract_broker_name(row['券商'])
+            if broker:
+                firmBP[broker] += round(float(row['價格']) * float(row['買進股數']) / 1000, 2)
+                firmBS[broker] += round(float(row['買進股數']) / 1000, 3)
+                firmSP[broker] += round(float(row['價格']) * float(row['賣出股數']) / 1000, 2)
+                firmSS[broker] += round(float(row['賣出股數']) / 1000, 3)
+            broker1 = extract_broker_name(row['券商.1'])
+            if broker1:
+                firmBP[broker1] += round(float(row['價格.1']) * float(row['買進股數.1']) / 1000, 2)
+                firmBS[broker1] += round(float(row['買進股數.1']) / 1000, 3)
+                firmSP[broker1] += round(float(row['價格.1']) * float(row['賣出股數.1']) / 1000, 2)
+                firmSS[broker1] += round(float(row['賣出股數.1']) / 1000, 3)
+        for j in firmBP.keys():
+            if firmBS[j] != 0:
+                firmBP[j] = round(firmBP[j] / firmBS[j], 2)
+                firmBS[j] = round(firmBS[j], 3)
+            else:
+                firmBP[j] = 0
+
+        for k in firmSP.keys():
+
+            if firmSS[k] != 0:
+                firmSP[k] = round(firmSP[k] / firmSS[k], 2)
+                firmSS[k] = round(firmSS[k], 3)
+            else:
+                firmSP[k] = 0
+        
+        write_to_excel()
+        close_window()
+    except Exception as e:
+        messagebox.showerror("錯誤", f"處裡錯誤: {e}")
+
+def write_to_excel():
+    # 将字典转换为 DataFrame
+    df_bp = pd.DataFrame(list(firmBP.items()), columns=['買入券商', '買入價格'])
+    df_bs = pd.DataFrame(list(firmBS.items()), columns=['買入券商', '買入張數'])
+    df_sp = pd.DataFrame(list(firmSP.items()), columns=['賣出券商', '賣出價格'])
+    df_ss = pd.DataFrame(list(firmSS.items()), columns=['賣出券商', '賣出張數'])
+
+    # 合并买入和卖出数据
+    df_buy = pd.merge(df_bp, df_bs, on='買入券商', how='outer').sort_values(by='買入張數', ascending=False)
+    df_sell = pd.merge(df_sp, df_ss, on='賣出券商', how='outer').sort_values(by='賣出張數', ascending=False)
+
+    # 创建一个ExcelWriter对象
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    output_file = os.path.join(desktop, f'{stock_code}_{formatted_date}.xlsx')
+    
+    with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
+        df_buy.to_excel(writer, sheet_name='買賣超', index=False, startrow=1)
+        df_sell.to_excel(writer, sheet_name='買賣超', index=False, startrow=1, startcol=len(df_buy.columns) + 1)
+    
+     # 调整列宽
+    workbook = load_workbook(output_file)
+    sheet = workbook['買賣超']
+
+    for col in sheet.columns:
+        column = col[0].column_letter  # 获取列字母
+        sheet.column_dimensions[column].width = 15
+
+    workbook.save(output_file)
+    
+    messagebox.showinfo("訊息", f"當日數據寫入Excel文件: {output_file}")
+
+root = tk.Tk()
+root.title("成交均價計算")
+
+# 设置窗口大小
+window_width = 300
+window_height = 200
+
+# 获取屏幕尺寸以计算窗口居中的位置
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# 计算窗口左上角的位置
+position_top = int(screen_height / 2 - window_height / 2)
+position_right = int(screen_width / 2 - window_width / 2)
+
+# 设置窗口的大小和位置
+root.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+
+# 创建一个框架来容纳按钮
+frame = tk.Frame(root)
+frame.grid(row=2, column=0, pady=20)
+
+# 添加文字标签
+label1 = tk.Label(frame, text="加入證交所個股交易量價CSV檔")
+label1.grid(row=0, column=0, columnspan=2, pady=(0, 10))
+
+# 添加第二个文字标签
+label2 = tk.Label(frame, text="This calculator-v1 powered by Michael")
+label2.grid(row=1, column=0, columnspan=2, pady=(0, 10))
+
+# 创建浏览按钮
+button = tk.Button(frame, text="瀏覽檔案", command=browse_file)
+button.grid(row=2, column=0, padx=10)
+
+# 创建离开按钮
+exit = tk.Button(frame, text="離開", command=close_window)
+exit.grid(row=2, column=1, padx=10)
+
+# 调整列和行的权重，使按钮框架在窗口中居中
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=0)
+root.grid_rowconfigure(2, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+root.mainloop()
+
